7b886fc41d562abc1bf344ea280574658e9f65da
Keeeeeeeevin <78458518+Keeeeeeeevin@users.noreply.github.com>
2021-02-02 18:59:15 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changecolor() {
+    document.getElementById("Kevin").style.color = "purple";
+}
+
+function changecolorwhite() {
+    document.getElementById("para").style.color = "white";
+}
